ef7892ed75617b3a59a676a3ae7f68ed830f15f5
Sama Mammadova <sema.m1997@gmail.com>
2022-05-23 03:17:09 +0300

    added checkout form components

--- cypress/components/checkoutForm.js
+++ cypress/components/checkoutForm.js
@@ -1,3 +1,17 @@
-export class checkoutForm {
+export class CheckoutForm {
+    static firstName() {
+        return cy.get('[data-test="firstName"]')
+    }
 
+    static lastName() {
+        return cy.get('[data-test="lastName"]')
+    }
+
+    static postalCode() {
+        return cy.get('[data-test="postalCode"]')
+    }
+
+    static continueButton() {
+        return cy.get('[data-test="continue"]')
+    }
 }
--- cypress/integration/purchase-flow-validation.spec.js
+++ cypress/integration/purchase-flow-validation.spec.js
@@ -1,3 +1,5 @@
+import {CheckoutForm} from "../components/CheckoutForm";
+
 describe('Purchase flow validation', () => {
     beforeEach(() => {
         cy.visit('/')
@@ -12,10 +14,10 @@ describe('Purchase flow validation', () => {
         //checkout
         cy.get('[data-test="checkout"]').click()
         //fill in fields
-        cy.get('[data-test="firstName"]')
-        cy.get('[data-test="lastName"]').type("Mammadova")
-        cy.get('[data-test="postalCode"]').type("11111")
-        cy.get('[data-test="continue"]').click()
+        CheckoutForm.firstName()
+        CheckoutForm.lastName().type("Mammadova")
+        CheckoutForm.postalCode().type("11111")
+        CheckoutForm.continueButton().click()
         cy.get('[data-test="error"]').should('contain.text', "Error: First Name is required")
 
     })
@@ -28,10 +30,10 @@ describe('Purchase flow validation', () => {
         //checkout
         cy.get('[data-test="checkout"]').click()
         //fill in fields
-        cy.get('[data-test="firstName"]').type("Sama")
-        cy.get('[data-test="lastName"]')
-        cy.get('[data-test="postalCode"]').type("11111")
-        cy.get('[data-test="continue"]').click()
+        CheckoutForm.firstName().type("Sama")
+        CheckoutForm.lastName()
+        CheckoutForm.postalCode().type("11111")
+        CheckoutForm.continueButton().click()
         cy.get('[data-test="error"]').should('contain.text', "Error: Last Name is required")
     })
 
@@ -43,10 +45,10 @@ describe('Purchase flow validation', () => {
         //checkout
         cy.get('[data-test="checkout"]').click()
         //fill in fields
-        cy.get('[data-test="firstName"]').type("Sama")
-        cy.get('[data-test="lastName"]').type("Mammadova")
-        cy.get('[data-test="postalCode"]')
-        cy.get('[data-test="continue"]').click()
+        CheckoutForm.firstName().type("Sama")
+        CheckoutForm.lastName().type("Mammadova")
+        CheckoutForm.postalCode()
+        CheckoutForm.continueButton().click()
         cy.get('[data-test="error"]').should('contain.text', "Error: Postal Code is required")
     })
 
@@ -58,10 +60,10 @@ describe('Purchase flow validation', () => {
         //checkout
         cy.get('[data-test="checkout"]').click()
         //fill in fields
-        cy.get('[data-test="firstName"]').type(" ")
-        cy.get('[data-test="lastName"]').type("Mammadova")
-        cy.get('[data-test="postalCode"]').type("11111")
-        cy.get('[data-test="continue"]').click()
+        CheckoutForm.firstName().type(" ")
+        CheckoutForm.lastName().type("Mammadova")
+        CheckoutForm.postalCode().type("11111")
+        CheckoutForm.continueButton().click()
         cy.location('pathname').should('not.be.eq', '/checkout-step-two.html')
     })
 
@@ -73,10 +75,10 @@ describe('Purchase flow validation', () => {
         //checkout
         cy.get('[data-test="checkout"]').click()
         //fill in fields
-        cy.get('[data-test="firstName"]').type("Sama")
-        cy.get('[data-test="lastName"]').type(" ")
-        cy.get('[data-test="postalCode"]').type("11111")
-        cy.get('[data-test="continue"]').click()
+        CheckoutForm.firstName().type("Sama")
+        CheckoutForm.lastName().type(" ")
+        CheckoutForm.postalCode().type("11111")
+        CheckoutForm.continueButton().click()
         cy.location('pathname').should('not.be.eq', '/checkout-step-two.html')
     })
 
@@ -88,10 +90,10 @@ describe('Purchase flow validation', () => {
         //checkout
         cy.get('[data-test="checkout"]').click()
         //fill in fields
-        cy.get('[data-test="firstName"]').type("Sama")
-        cy.get('[data-test="lastName"]').type("Mammadova")
-        cy.get('[data-test="postalCode"]').type(" ")
-        cy.get('[data-test="continue"]').click()
+        CheckoutForm.firstName().type("Sama")
+        CheckoutForm.lastName().type("Mammadova")
+        CheckoutForm.postalCode().type(" ")
+        CheckoutForm.continueButton().click()
         cy.location('pathname').should('not.be.eq', '/checkout-step-two.html')
     })
 })
